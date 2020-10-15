00fc0c32923fa401d0b330b04cd078a54a78aeec
Emily Bearden <emily.bearden@yahoo.com>
2020-10-15 22:20:31 +0000

    Completed assignment

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main {
   public static void main(String[] args) {
     // test your code here by creating a three different dogs from the Dog class. Be sure you give each dog different characteristics. 
+ Dog Cupcake = new Dog("Cupcake", "mixed", 70, "black", true);
+
+ System.out.println(Cupcake);
+
+ Dog Sprinkles= new Dog("Sprinkles", "mixed", 35, "white and black", true);
+    
+System.out.println(Sprinkles);
+
+Dog Toto = new Dog("Toto", "Cairn Terrier", 15, "brown", false);
+
+System.out.println(Toto);
   }
 }
